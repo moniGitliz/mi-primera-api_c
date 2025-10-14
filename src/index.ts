72c84af7e93b5c214428110f045d3f0456f037b7
jtorrpab <juanpablotorresp.17@gmail.com>
2025-10-14 11:59:15 -0500

    ejercicio sobre  promesas y async-await

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-import { startServer } from "./api";
+// import { startServer } from "./api";
 
-startServer();
+import { PollingWatchKind } from "typescript";
+
+// startServer();
+
+// Promesas
+const getPockemonById = (id: number) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((pokemon: any) => pokemon.name);
+};
+
+// let miPokemon = getPockemonById(1);
+// console.log({ miPokemon });
+
+getPockemonById(1)
+  .then((nombre) => console.log("El Pokémon es:", nombre))
+  .catch((error) => console.error("Error al obtener el Pokémon:", error));
+
+//  async - await
+
+const getPockemonById2 = async (id: number) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const respuesta = await fetch(url);
+  const pokemon = (await respuesta.json()) as { name: string };
+  return pokemon.name;
+};
+
+getPockemonById2(2).then((pockemonName) => {
+  console.log({ pockemonName });
+});
